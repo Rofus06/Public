55f8e53e9039b15bd6d4f8de82ae85231dc3c5a0
Rofus06 <Filipsmail@yahoo.com>
2023-08-30 10:42:33 +0200

    Luck Text

---
+++ 22_luck/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("what is your name?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Hey" + name + "Were do you live?");
+
+string name2 = Console.ReadLine();
+
+Console.WriteLine("ok" + name2 + "got it, be more spesific");
+
+string namee = Console.ReadLine();
+
+Console.WriteLine("ok" + namee + "thx for you info");
+
+Console.ReadLine();
